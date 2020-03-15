24d14862f8ea341a6d97f57d8ec2eb6adcdca411
ShuogeShen <soapgoodchild@hotmail.com>
2020-03-15 15:00:44 -0400

    Updates

---
+++ precache-manifest.058bc5134d8b2d71f3bd0b89144da0f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804c7d6dad8383fa9cc483f590acf3b0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f2068183b7e7603c9cba",
+    "url": "/monsters-rolodex/static/css/main.f653b258.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2068183b7e7603c9cba",
+    "url": "/monsters-rolodex/static/js/main.85254cc2.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
